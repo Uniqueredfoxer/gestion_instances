8c5bb9156bb65a10dc72d22b6436d0c5b564a8c2
adam <derboadama31@gmail.com>
2026-06-21 21:11:42 +0000

    fixed

--- frontend/app/hooks/useAuth.tsx
+++ frontend/app/hooks/useAuth.tsx
@@ -12,7 +12,8 @@ interface AuthContextType {
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://directtrack.onrender.com/api';
+if(!API_URL)console.log('environment variable not loaded...')
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
